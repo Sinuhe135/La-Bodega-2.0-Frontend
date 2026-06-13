c8e18957d6d6804f5d87a0a70f0886f0cfb9f5ba
Sinuhe135 <sinuhe9999@gmail.com>
2026-06-13 13:36:17 -0700

    Mockup of account header

--- src/pages/app/index.vue
+++ src/pages/app/index.vue
@@ -13,6 +13,8 @@ onMounted(() => {
   // onPageLoad()
 })
 
+const selectedRole = ref('admin')
+
 // const onPageLoad = async () => {
 //   const userData = await getCurrentUserApi();
 //   username.value = userData.username;
@@ -20,9 +22,56 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-    <VCard>
-      Hello, {{ username }}
+  <VCard class="accounts-header">
+
+      <div id="category-container">
+        <VControl>
+          <VSelect v-model="selectedRole">
+            <option value="admin">Admin</option>
+            <option value="editor">Editor</option>
+            <option value="viewer">Viewer</option>
+          </VSelect>
+        </VControl>
+      </div>
+
+      <div id="search-container">
+        <VControl class="search-input">
+          <VInput placeholder="Search accounts..." />
+        </VControl>
+        <VIconButton color="primary" light outlined icon="material-symbols:search"/>
+      </div>
+
+      <div id="new-container">
+        <VButton color="primary">New Account</VButton>
+      </div>
     </VCard>
-  </div>
 </template>
+
+<style lang="scss" scoped>
+  .accounts-header {
+    display: flex;
+    gap: 1rem;
+
+    #category-container {
+      flex: 1;
+    }
+
+    #search-container {
+      flex: 2;
+
+      display: flex;
+      gap: 0.3rem;
+
+      .search-input {
+        flex: 1;
+      }
+    }
+
+    #new-container {
+      flex: 1;
+
+      display: flex;
+      justify-content: center;
+    }
+  }
+</style>
